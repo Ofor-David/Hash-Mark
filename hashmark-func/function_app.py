c80ba21dbd60907fb95bd55e72438ad31c3dc50d
Ofor David <davidoforincloud@gmail.com>
2025-09-03 14:28:36 +0100

    function code update & fix

--- hashmark-func/function_app.py
+++ hashmark-func/function_app.py
@@ -1,27 +1,22 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+import hashlib
 
 app = func.FunctionApp()
 
+@app.blob_trigger(arg_name="myblob", path="uploads/{name}",connection="AzureWebJobsStorage") 
+def main(myblob: func.InputStream):
+     logging.info(f"Python blob trigger function processed blob"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
+     
+     # Read blob content
+     file_bytes = myblob.read()
+     
+     # Compute SHA-256 hash
+     file_hash = hashlib.sha256(file_bytes).hexdigest()
+     
+     logging.info(f"SHA-256 Hash: {file_hash}")
 
-@app.blob_trigger(arg_name="myblob", path="uploads/{name}",
-                               connection="AzureWebJobsStorage") 
-def BlobHashFunction(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
 
-
-# This example uses SDK types to directly access the underlying BlobClient object provided by the Blob storage trigger.
-# To use, uncomment the section below and add azurefunctions-extensions-bindings-blob to your requirements.txt file
-# import azurefunctions.extensions.bindings.blob as blob
-# @app.blob_trigger(arg_name="client", path="uploads/{name}",
-#                   connection="AzureWebJobsStorage")
-# def BlobHashFunction(client: blob.BlobClient):
-#     logging.info(
-#         f"Python blob trigger function processed blob \n"
-#         f"Properties: {client.get_blob_properties()}\n"
-#         f"Blob content head: {client.download_blob().read(size=1)}"
-#     )
+# Store the hash in a table
